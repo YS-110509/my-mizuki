6198a20bb3feb815f4fbc78ef1d98db720dd7237
桜島芦花 <2035566353@qq.com>
2026-02-05 01:11:17 +0800

    修复 config.ts title错误

--- src/config.ts
+++ src/config.ts
@@ -22,7 +22,7 @@ import { LinkPreset } from "./types/config";
 const SITE_LANG = "zh_CN"; // 语言代码，例如：'en', 'zh_CN', 'ja' 等。
 const SITE_TIMEZONE = 8; //设置你的网站时区 from -12 to 12 default in UTC+8
 export const siteConfig: SiteConfig = {
-	title: "Roka's room,
+	title: "Roka's room",
 	subtitle: "芦花の小屋",
 	siteURL: "https://www.roka.ink/", // 请替换为你的站点URL，以斜杠结尾
 	siteStartDate: "2025-08-08", // 站点开始运行日期，用于站点统计组件计算运行天数
