c0d94d8ce9836ca6c42252e162d8e368a6e816dc
桜島芦花 <2035566353@qq.com>
2026-02-25 00:31:37 +0800

    更新 friends.ts 友链

--- src/data/friends.ts
+++ src/data/friends.ts
@@ -12,7 +12,30 @@ export interface FriendItem {
 
 // 友情链接数据
 export const friendsData: FriendItem[] = [
-	
+	{
+		id: 1,
+		title: "星之彼岸系社",
+		imgurl: "https://society.gimovo.com/favicon.ico",
+		desc: "欢迎了解星之彼岸系社团，我们简称为星彼社。",
+		siteurl: "https://society.gimovo.com/",
+		tags: ["社团", "线下活动", "ACGN"],
+	},
+	{
+		id: 2,
+		title: "星网",
+		imgurl: "https://gimovo.com/favicon.ico",
+		desc: "由GIMOVO STUDIO制作并管理的大型综合性论坛。",
+		siteurl: "https://society.gimovo.com/",
+		tags: ["论坛", "音乐讨论", "游戏讨论", "美术讨论"],
+	},
+	{
+		id: 3,
+		title: "大上海城ACGN交流群",
+		imgurl: "https://dshcacg.fun/images/logo.jpg",
+		desc: "郑州大上海城二次元交流群的初代官网，目前该版本已停止更新。",
+		siteurl: "https://dshcacg.fun/",
+		tags: ["ACGN", "线下活动", "社区"],
+	},
 ];
 
 // 获取所有友情链接数据
